a1940f6a5df6021998d9f104250233e2e7b194ce
rjspyk5 <rjspyk5@gmail.com>
2024-03-23 15:49:17 +0600

    routing setup complete

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Root } from "./Component/Root/Root";
+import { Home } from "./Component/Home/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [{ path: "/", element: <Home /> }],
   },
 ]);
